b5d4071e10c62db792ac224afd11ed4792478193
Sungwon Jude Park <psw0616a@naver.com>
2023-09-22 16:43:17 +0900

    Example 8 Update

---
+++ src/week3/CompOperator.java
@@ -0,0 +1,13 @@
+package week3;
+
+public class CompOperator {
+    public static void main(String[] args) {
+        System.out.print((3 == 4) + " ");
+        System.out.print((3 != 4) + " ");
+        System.out.print((3 > 4) + " ");
+        System.out.print((4 > 3) + " ");
+
+        System.out.print((3 == 4 &&  4 ==7 ) + " ");
+        System.out.println((3 == 4 ||  4 ==7 ) + " ");
+    }
+}
